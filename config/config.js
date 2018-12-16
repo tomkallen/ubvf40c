36af8d56c49a13d0c9d887c43dc4e91504821c29
tomkallen <comradebykoff@gmail.com>
2018-12-16 23:55:46 +0300

    first set up

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://tomkallen:Tom9219433021@tn-wrjlk.mongodb.net/test?retryWrites=true'
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const config = require('./config/config')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+mongoose
+  .connect(
+    config.mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch(err => console.log(err))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server up and running on port ${port} !`))
